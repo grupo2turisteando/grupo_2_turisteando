425226ab445c5fd241c32601d7141a0db8d791bd
Facundo Minuto <facu@MacBook-Pro-de-Facundo.local>
2022-04-09 19:04:54 -0300

    subiendo HTML de Detalle de Producto + CSS

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
+app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'));
 });
 
