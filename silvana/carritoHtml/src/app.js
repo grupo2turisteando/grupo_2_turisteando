c9fa26a79ea0ff8c9bab382cd50da1299c0f5937
silacotto <silacotto@gmail.com>
2022-04-09 11:13:43 -0300

    comienzo html datos compra : pasajeros, medio pago

--- silvana/carritoHtml/src/app.js
+++ silvana/carritoHtml/src/app.js
@@ -19,6 +19,9 @@ app.get('/productDetail', (req, res) => {
 app.get('/productCart', (req, res) => {
     res.sendFile(path.join(__dirname, './views/productCart.html'));
 });
+app.get('/paymentForm', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/paymentForm.html'));
+});
 
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'));
