430f9393775efd7d92e428865930e4081fdbbd4f
Facundo Minuto <facu@MacBook-Pro-de-Facundo.local>
2022-05-07 17:27:06 -0300

    dinamizando packages y deals

--- src/controllers/packages_controller.js
+++ src/controllers/packages_controller.js
@@ -1,9 +1,12 @@
 /* Este controlador "packages_controller" implementa todos los metodos para
 manejar los productos */
 
+/* cargo el manejador de los paquetes del modelo */
+const data_paquetes = require('../model/data_paquetes.js');
+
 const packages_controller = {
     packages: (req, res) => {
-        res.status(200).render('../views/packages');
+        res.status(200).render('../views/packages', {data_paquetes: data_paquetes });
     }
 };
 
