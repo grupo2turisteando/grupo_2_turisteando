0287b6a6f8676a50de3d6fb28532062ea990065f
Facundo Minuto <facu@MacBook-Pro-de-Facundo.local>
2022-05-09 20:55:43 -0300

    modificaciones

--- src/controllers/products_controller.js
+++ src/controllers/products_controller.js
@@ -1,22 +1,8 @@
 /* Este controlador "products_controller" implementa todos los metodos para
 manejar los productos */
 
-// const products_controller = {
-//     packages: (req, res) => {
-//         res.status(200).render('../views/packages');
-//     },
-//     deals: (req, res) => {
-//         res.status(200).render('../views/deals');
-//     },
-//     productDetail: (req, res) => {
-//         res.status(200).render('../views/productDetail');
-//     }
-// };
-
-// module.exports = products_controller;
-
 const access_database = require('../model/access_database.js');
-const engine_json = require('../model/engine_json.js');
+const enginen = require('../model/engine.js');
 
 const products_controller = {
     productDetail: (req, res) => {
