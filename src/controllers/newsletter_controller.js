66b8c8d6d8b16ac8dad7caeebd7bcc09700de1cf
Facundo Minuto <facu@MacBook-Pro-de-Facundo.local>
2022-05-15 20:14:09 -0300

    ruteo newsletter

--- src/controllers/newsletter_controller.js
+++ src/controllers/newsletter_controller.js
@@ -5,16 +5,39 @@ const path = require('path');
 const engine = require('../model/engine.js');
 
 /* defino las variables del entorno */
-const path_relative = path.join(__dirname, '../../data')
+//const path_relative = path.join(__dirname, '../../data')
+
+// const newsletter_controller = {
+//     new_register: (req, res) => {
+//         let register = req.body
+
+//         let registers = engine.add_columm("newsletter", register);
+//        // console.log(register);
+//        // res.status(200).redirect('/'); //ruta de cierre del metodo POST
+//     }
+// };
 
 const newsletter_controller = {
     new_register: (req, res) => {
-        let register = req.body
+        let register = req.body;
 
-        let registers = engine.add_columm("newsletter", register);
-       // console.log(register);
-       // res.status(200).redirect('/'); //ruta de cierre del metodo POST
-    }
-};
+            /* Guardar registro */
+            let newsletter_file = fs.readFileSync('data/newsletter.json', {encoding: 'utf-8'});
+            let registers;
+            if(newsletter_file == "") {
+                registers = [];
+            } else {
+                registers = JSON.parse(newsletter_file);
+            };
+
+            registers.push(register);
+
+            registersJSON = JSON.stringify(registers);
+
+            fs.writeFileSync('data/newsletter.json', registersJSON);
+
+            res.status(200).redirect('/'); //ruta de cierre del metodo POST
+        }
+    };
 
 module.exports = newsletter_controller;
