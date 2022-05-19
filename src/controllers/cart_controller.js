4db58839cf9844134817574880ca9a0758c4cc29
silacotto <silacotto@gmail.com>
2022-05-19 18:31:12 -0300

    comienzo a validar form cart

--- src/controllers/cart_controller.js
+++ src/controllers/cart_controller.js
@@ -2,27 +2,14 @@ const access_database= require('../model/access_database.js');
 const { browse_table, write_json } = require('../model/engine.js');
 const engine= require('../model/engine.js'); // con este modulo operamos la base de datos
 
-/* write_json: function (name_table, file_string){
-        let file_json= JSON.stringify(file_string);
-        let id = 0
-        if (file_json.length>0){
-        id=file_json.length + 1;
-        }
-        fs.writeFileSync(`${path_relative + name_table}.json`,file_json, 
-                        err => (err)? console.log('ERROR al grabar archivo JSON'): '');
-    },
- */
-
+const {validationResult}= require("express-validator");
 
 
 const cart_controller = {
     show_cart :  (req, res) => {
-        let data_show_cart= engine.browse_table("cart");
-
-        
-       res.status(200).render('../views/productCart', {data_show_cart:data_show_cart});
-        
-    },
+            let data_show_cart= engine.browse_table("cart");
+            res.status(200).render('../views/productCart', {data_show_cart:data_show_cart});
+            },
       
     add_item :   (req, res) => {
        
@@ -40,6 +27,22 @@ const cart_controller = {
         engine.delete_columm("cart", data_delete_cart)        
         res.redirect("/cart")          
             },
+        
+    cart_form :  (req, res) => {
+             res.render("../views/cartForm");
+             },
+
+    process_cart_form: (req,res)=>{
+       const result_validation_cart = validationResult(req);
+ 
+       
+       if (result_validation_cart.errors.length > 0){
+           res.render("cartForm", {
+                errors : result_validation_cart.mapped()
+           })
+       }
+
+    },
     
     purchase : (req, res) => {
         res.send("Comprar");
@@ -49,9 +52,7 @@ const cart_controller = {
         res.send("Ver detalle de Compra");
             },
             
-    cart_form :  (req, res) => {
-                res.render("../views/cartForm");
-                    },
+   
     cart_final :  (req, res) => {
                     res.render("../views/cartFinal");
                             },
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,8 +4,36 @@ const path = require('path');
 const express= require('express');
 const router= express.Router(); /* Router permiete crear rutas montables y desmontables */
 /* el metodo HTTP es llamado desde Router */
+// validar formulario
+const {body} = require("express-validator")
+const validator_cart_form =[
+    //select
+    body("mayores").notEmpty().withMessage("Debes indicar cuantos mayores viajaran"), 
+    body("menores").notEmpty().withMessage("Debes indicar cuantos menores viajaran"), 
+
+    body("first_name").notEmpty().withMessage("Debes ingresar un nombre"), 
+    body("last_name").notEmpty().withMessage("Debes ingresar un apellido"), 
+    body("birth_date").notEmpty().isDate().withMessage("Debes ingresar una fecha de nacimiento"),
+    body("age").notEmpty().withMessage("Debes ingresar la edad"),
+    body("identity_document").notEmpty().isLength({min: 6 ,max:10}).withMessage("Debes ingresar un numero de documento"),
+    body("home").notEmpty().withMessage("Debes ingresar un domicilio"),
+    body("postal_code").notEmpty().withMessage("Debes ingresar un código postal"),
+    body("province").notEmpty().withMessage("Debes indicar una Provincia de residencia"),
+    body("email").isEmail().notEmpty().withMessage("Debes ingresar un email válido"),
+    body("phone_number").notEmpty().isLength({min: 6 ,max:10}).withMessage("Debes ingresar un número de teléfono valido"),
+  
+    //radio
+    body("payment_method").notEmpty().withMessage("Indica como quieres pagar"),
+    body("payment_method").notEmpty().withMessage("Indica como quieres pagar"),
+    body("card_number").notEmpty().isLength({min:16, max:16}).withMessage("Ingresa los 16 dígitos ubicados en el frente de la tarjeta"),
+    body("name_card_holder").notEmpty().withMessage("Ingresa el nombre del titular de la tarjeta"),
+    body("expiration").notEmpty().isLength({min:4, max:4}).withMessage("Ingresa la fecha de vencimiento de la tarjeta"),
+    body("security_code").notEmpty().isLength({min:3, max:4}).withMessage("Ingresa el código de seguridad de la tarjeta"),
+
+]
 
 const cart_controller = require('../controllers/cart_controller.js');
+const { default: isMobilePhone } = require('validator/lib/isMobilePhone');
 
 /* rutas con controladores */
 
@@ -26,8 +54,16 @@ router.get("/purchase", cart_controller.purchase);
 //detalle de compra//
 router.get("/purchaseDetail", cart_controller.purchase_detail );
 
-//detalle de compra//
+//formulario de compra/
 router.get("/cartForm", cart_controller.cart_form );
+//procesar formulario compra
+router.post("/cartForm",validator_cart_form, cart_controller.process_cart_form );
+
+//formulario de compra/
+router.get("/add/:id/cartForm", cart_controller.cart_form );
+
+
+
 //detalle de compra//
 router.get("/cartFinal", cart_controller.cart_final );
 
