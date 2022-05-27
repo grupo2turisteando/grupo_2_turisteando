038dfbcb7a1a1c55af9fba7cea2637485866e1ee
silacotto <silacotto@gmail.com>
2022-05-27 00:06:43 -0300

    trabajando en listado usuarios, detalle useuario, editar usuario

--- src/controllers/users_admin_controllers.js
+++ src/controllers/users_admin_controllers.js
@@ -1,12 +1,30 @@
 const { users_list } = require("./users_controller");
-const engine= require('../model/engine.js')
+const engine= require('../model/engine.js');
+const { find_columm } = require("../model/engine.js");
 
 const users_admin_controller = {
+
+    /*Nostrar todos usuarios Registrados*/
     users_list: (req, res) => {
+       
         let users_table= engine.browse_table('users-prueba');
         res.status(200).render('../views/users_admin', {users_table: users_table});
+    },
+    show_user :  (req, res) => {
+        let user_id= req.params.id;
+        let user= engine.read_columm('users-prueba', user_id); 
+        res.status(200).render("../views/user_admin_detail", {user: user[0]}); 
+           
+        },
+        
+      
+      
+    edit_user_get: (req, res) => {
+        let user_id= req.params.id;
+        let user= engine.read_columm('users-prueba', user_id); 
+        res.status(200).render("../views/users/users_edit", {user: user[0]}); //como envio un objeto literal uso el indice cero del array
     }
-    }
+}
 
 
 
