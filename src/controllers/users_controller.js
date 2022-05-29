beae16c1327ecc3391efb54f5de33ad77c31ddd9
IgnacioParisi <parisi.ignacio@gmail.com>
2022-05-29 16:26:53 -0300

    agregando registro completo

--- src/controllers/users_controller.js
+++ src/controllers/users_controller.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
-const path= require('path');
-const { check, validationResult, body } = require('express-validator');
+const path = require('path');
+const { validationResult } = require('express-validator');
 const engine = require('../model/engine.js');
 
+const bcryptjs = require('bcryptjs');
 
+const User = require('../model/Users.js');
 
 
 const users_controller = {
     login: (req, res) => {
         res.status(200).render('../views/users/login')
     },
+    
     process_login: (req, res) => {
         let errors = validationResult(req);
         if (errors.isEmpty()) {
@@ -23,7 +26,7 @@ const users_controller = {
 
             for (let i = 0; i < users.length; i++) {
                 if (users[i].email == req.body.email) {
-                    if ( bcrypt.compareSync(req.body.password, users[i].password)) {
+                    if ( bcryptjs.compareSync(req.body.password, users[i].password)) {
                         let login_user = users[i];
                         break;
                     }
@@ -42,84 +45,51 @@ const users_controller = {
             return res.render('../views/users/login', {errors: errors.errors});
         }
     },
+
     register: (req, res) => {
         res.status(200).render('../views/users/register')
     },
+
     add_user: (req, res) => {
         const result_validation = validationResult(req);
-        if (result_validation.isEmpty()) {
-            let user = {
-                    id: 0,
-                    nombre: req.body.usuario,
-                    email: req.body.email,
-                    password: req.body.password,
-                    password_valid: req.body.password_valid,
-                    avatar: req.body.avatar
-                };
-
-            /* Guardar usuario*/
-            /** Leer JSON */
-            let users_file = fs.readFileSync('data/users-prueba.json', {encoding: 'utf-8'});
-            let users;
-            if(users_file == "") {
-                users = [];
-            } else {
-                users = JSON.parse(users_file);
-            };
-
-            /** Agregar id user */ 
-            user.id = users.length + 1;
-
-            users.push(user);
-
-            usersJSON = JSON.stringify(users);
-            /** Reescribir JSON */
-            fs.writeFileSync('data/users-prueba.json', usersJSON);
-            res.status(200).redirect('/users/login');
-        } else {
-            res.render('../views/users/register', { errors: result_validation.mapped(), oldData: req.body }); //result_validation.array()
+        if (result_validation.errors.length > 0) {
+            return res.render('../views/users/register', {
+                errors: result_validation.mapped(),
+                oldData: req.body
+            });
         }
-        // if (result_validation.errors.length > 0) {
-        //     return res.render('../views/users/register'), {
-        //         errors: result_validation.mapped(),
-        //         oldData: req.body
-        //     }
-        // }
-        
-        // let user = {
-        //     id: 0,
-        //     nombre: req.body.usuario,
-        //     email: req.body.email,
-        //     password: req.body.password,
-        //     password_valid: req.body.password_valid
-        // };
+        // Chequea si el email ya está registrado en otro usuario
+        let user_in_db = User.find_by_field('email', req.body.email);
 
-        // /* Guardar usuario*/
-        // /** Leer JSON */
-        // let users_file = fs.readFileSync('data/users-prueba.json', {encoding: 'utf-8'});
-        // let users;
-        // if(users_file == "") {
-        //     users = [];
-        // } else {
-        //     users = JSON.parse(users_file);
-        // };
+        if (user_in_db) {
+            return res.render('../views/users/register', {
+                errors: {
+                    email: {
+                        msg: 'Este email ya está registrado'
+                    }
+                },
+                oldData: req.body
+            });
+        }
+        //Crea el usuario con la encriptacion de la password
+        let user_to_create = {
+            ...req.body,
+            password: bcryptjs.hashSync(req.body.password, 10),
+            password_valid: bcryptjs.hashSync(req.body.password_valid, 10),
+            avatar: req.file.filename
+            }
 
-        // /** Agregar id user */ 
-        // user.id = users.length + 1;
-
-        // users.push(user);
-
-        // usersJSON = JSON.stringify(users);
-        // /** Reescribir JSON */
-        // fs.writeFileSync('data/users-prueba.json', usersJSON);
-
-        // res.status(200).redirect('/users/login'); //ruta de cierre del metodo POST
+            let user_created = User.create(user_to_create);
+            
+            return res.status(200).redirect('/users/login');
     },
+
     users_list: (req, res) => {
         let users_list = fs.readFileSync('users-prueba.json', {encoding: 'utf-8'});
         let users = JSON.parse(users_list);
         res.status(200).render('users_list', {'users': users})
     },
+
     view_users: (req, res) => {
         res.status(200).render('../views/users_admin')
     }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,60 +1,61 @@
 //  Sistema de ruteo Usuarios  //
-
 const path = require('path');
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const { check, validationResult, body } = require('express-validator');
-
 const users_controller = require('../controllers/users_controller');
 
- // Multer */
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../../public/images/avatars'));
-    },
-    filename: (req, file, cb) => {
-        let file_name = `${Date.now()}_img${path.extname(file.originalname)}`;  
-        cb(null, file_name);
-    }
-})
+const validations = require('../middlewares/validation_middleware');
+const uploadFile = require('../middlewares/multer_middleware');
 
-const uploadFile = multer({ storage });
+//  // Multer */
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, path.join(__dirname, '../../public/images/users/avatars'));
+//     },
+//     filename: (req, file, cb) => {
+//         let file_name = `${Date.now()}_img${path.extname(file.originalname)}`;  
+//         cb(null, file_name);
+//     }
+// })
 
-// Validaciones
-const validations = [
-    body('user').notEmpty().withMessage('Tienes que escribir un nombre'),
-    body('email')
-        .notEmpty().withMessage('Tienes que escribir un email').bail()
-        .isEmail().withMessage('Debes escribir un formato de correo válido'),
-    body('password')
-        .notEmpty().withMessage('Tienes que escribir una contraseña').bail()
-        .isLength( {min: 8} ).withMessage('La contraseña debe tener como mínimo 8 caracteres'),
-    body('password_valid')
-        .notEmpty().withMessage('Tienes que repetir la contraseña').bail()
-        .custom((value, { req }) => {
-            if (value !== req.body.password) {
-              throw new Error('La contraseña de verificación no coincide. Intentalo de nuevo ');
-            }
+// const uploadFile = multer({ storage });
 
-            return true;
-          }),
-    body('avatar').custom((value, { req }) => {
-        let file = req.file;
-        let acceptedExtensions = ['.jpg', '.png'];
+// // Validaciones
+// const validations = [
+//     body('user').notEmpty().withMessage('Tienes que escribir un nombre'),
+//     body('email')
+//         .notEmpty().withMessage('Tienes que escribir un email').bail()
+//         .isEmail().withMessage('Debes escribir un formato de correo válido'),
+//     body('password')
+//         .notEmpty().withMessage('Tienes que escribir una contraseña').bail()
+//         .isLength( {min: 8} ).withMessage('La contraseña debe tener como mínimo 8 caracteres'),
+//     body('password_valid')
+//         .notEmpty().withMessage('Tienes que repetir la contraseña').bail()
+//         .custom((value, { req }) => {
+//             if (value !== req.body.password) {
+//               throw new Error('La contraseña de verificación no coincide. Intentalo de nuevo ');
+//             }
+
+//             return true;
+//           }),
+//     body('avatar').custom((value, { req }) => {
+//         let file = req.file;
+//         let acceptedExtensions = ['.jpg', '.png', '.jpeg'];
         
-        if (!file) {
-            throw new Error('Tienes que subir una imagen');
-        } else {
-            let fileExtension = path.extname(file.originalname);
-            if (!acceptedExtensions.includes(fileExtension)) {
-                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
-        }
-        }
+//         if (!file) {
+//             throw new Error('Tienes que subir una imagen');
+//         } else {
+//             let fileExtension = path.extname(file.originalname);
+//             if (!acceptedExtensions.includes(fileExtension)) {
+//                 throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+//         }
+//         }
     
-        return true;
-    })
-];
+//         return true;
+//     })
+// ];
 
 /* Rutas controladores usuarios */
 
