5dad68df371ab16b67c78a0c9e6b7edde02232bc
wleskovar <wleskovar@gmail.com>
2022-06-26 15:19:54 -0300

    modificado el crear paquete, no pide mas el id, lo asigna la base de datos, se modifico el formulario de alta y las validacions

--- src/middlewares/validations_packge.js
+++ src/middlewares/validations_packge.js
@@ -4,9 +4,6 @@ const { body } = require('express-validator'); // la funcion body es igual a la 
 
 // defino el array con las validaciones del express-validator
 const validations_package= [
-    body('package_id')
-        .notEmpty().withMessage('El código del paquete es obligatorio').bail()
-        .isNumeric().withMessage('El código del paquete debe ser númerico'),
     body('package_name').notEmpty().withMessage('Ingresar el nombre del paquete'),
     body('package_alt_image').notEmpty().withMessage('Ingresar la leyenda de la imagen del paquete'),
     body('package_price')
@@ -16,13 +13,8 @@ const validations_package= [
     body('package_q_days')
         .notEmpty().withMessage('Ingresar la cantidad de días de alojamiento').bail()
         .isNumeric().withMessage('El valor a ingresar es númerico'),
-    body('package_hotel').notEmpty().withMessage('El nombre del Hotel del paquete es obligatorio'),
-    body('package_stars_hotel')
-        .notEmpty().withMessage('Ingresar la categoria del hotel en estrellas').bail()
-        .isNumeric().withMessage('El valor a ingresar es númerico'),
     body('package_description').notEmpty().withMessage('Ingrese la descripción del paquete para el usuario'),
     body('package_date_admission').notEmpty().withMessage('Ingrese la fecha de ingreso al hotel'),
-    body('package_excursions_id').notEmpty().withMessage('Ingrese cero si no tiene excursiones incluidas o el codigo del paquete de excursiones'),
     body('package_discount')
         .notEmpty().withMessage('Ingrese el porcentaje de descuento sobre el precio del paquete').bail()
         .isNumeric().withMessage('El valor a ingresar es númerico'),
