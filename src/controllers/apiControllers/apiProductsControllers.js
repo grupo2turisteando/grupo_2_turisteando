c7d5de9e1f94bf24663d9d9a01fa3abdc5b12aa9
Facundo Minuto <facu@MacBook-Pro-de-Facundo.local>
2022-08-06 16:09:33 -0300

    endpoint + controlador p/listar cant de productos en DB

--- src/controllers/apiControllers/apiProductsControllers.js
+++ src/controllers/apiControllers/apiProductsControllers.js
@@ -1,12 +1,26 @@
+const { get } = require("express/lib/request");
 const db = require("../../../database/models");
 const sequelize = db.sequelize;
 
 
 const apiProductsControllers = {
 
-    list: (req,res)=>{
-      
-    
+    list: (req,res) => {
+        db.Producto.findAll({
+            attributes: [
+                [sequelize.fn('COUNT', sequelize.col('package_id')),"packages"]
+            ]
+        })
+        .then(product => {
+            return res.json({
+                meta: {
+                    method: "get",
+                    status: 200,
+                    url: "http://localhost:5020/api/packages"
+                },
+                data: product,              
+            })
+        })
     },
     detail: (req,res)=>{
     
