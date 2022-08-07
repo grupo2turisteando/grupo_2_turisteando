edd0a7f34e413e9312cd90117a1b85dd58b1cb04
Facundo Minuto <facu@MacBook-Pro-de-Facundo.local>
2022-08-07 14:36:33 -0300

    endpoint y controller funcionales p/totalPaquetes en Dashboard

--- src/routes/routesApi/apiProducts.js
+++ src/routes/routesApi/apiProducts.js
@@ -1,13 +1,14 @@
 /* Sistema de Ruteo */
 const path = require('path');
 const express= require('express');
+const cors = require('cors');
 
 const router= express.Router(); /* Router permiete crear rutas montables y desmontables */
 
 const apiProductsControllers = require('../../controllers/apiControllers/apiProductsControllers');
 
 
-router.get('/', apiProductsControllers.list);//Facundo
+router.get('/', cors(), apiProductsControllers.list);//Facundo
 router.get('/:id', apiProductsControllers.detail);//Walter
 
 
