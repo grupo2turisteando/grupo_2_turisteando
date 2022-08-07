06dea4104869c4a4d286c6a980fbdb7afbf0956a
Facundo Minuto <facu@MacBook-Pro-de-Facundo.local>
2022-08-07 20:44:52 -0300

    revisiones al codigo

--- src/controllers/apiControllers/apiProductsControllers.js
+++ src/controllers/apiControllers/apiProductsControllers.js
@@ -23,7 +23,21 @@ const apiProductsControllers = {
         })
     },
     detail: (req,res)=>{
-    
+        db.Producto.findAll({
+            attributes: [
+                ["package_name", "producto"]
+            ]
+        })
+        .then(product => {
+            return res.json({
+                meta: {
+                    method: "get",
+                    status: 200,
+                    url: "http://localhost:5020/api/products/:id"
+                },
+                data: product,              
+            })
+        })
     },
    
     };
